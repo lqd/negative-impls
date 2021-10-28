ff3674105cf6bb2b2f1749e86682f5273097af05
Remy Rakic <remy.rakic+github@gmail.com>
2021-10-28 21:29:30 +0200

    add working version

--- fake_libcore/src/lib.rs
+++ fake_libcore/src/lib.rs
@@ -2,5 +2,6 @@
 
 pub trait Future {}
 
-impl<E> !Future for Option<E> {} // version A: breaks fake_libstd
-//impl !Future for Option<()> {} // version B: works
+// impl<E> !Future for Option<E> {} // version A: breaks fake_libstd
+// impl !Future for Option<()> {} // version B: works
+impl<E: ?Sized> !Future for Option<E> {} // version C: works
